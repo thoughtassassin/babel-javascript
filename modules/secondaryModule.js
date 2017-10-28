f58cd61788b71a77e37c9f4549311ed61ae3d130
David Krause <david@david-4.local>
2017-10-28 13:00:06 -0500

    Fix formatting

--- modules/secondaryModule.js
+++ modules/secondaryModule.js
@@ -1,8 +1,8 @@
 import {tertiaryModule} from './tertiaryModule.js'
 
 export const testModule = {
-	printMessage: message => console.log("From pringMessage: " + message),
-	logAnything: message => console.log("From log anything: " + message),
+  printMessage: message => console.log("From pringMessage: " + message),
+  logAnything: message => console.log("From log anything: " + message),
   saySomething: tertiaryModule.saySomething,
   anotherModule: tertiaryModule.doWhatever,
   composeChange: tertiaryModule.oneMoreTest
